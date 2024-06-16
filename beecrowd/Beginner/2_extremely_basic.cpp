d73bded83c2e3ee95a08f47a6f77927dcda0311e
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-06-16 22:07:56 +0600

    Create 2_extremely_basic.cpp

---
+++ beecrowd/Beginner/2_extremely_basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, X; // input1, input2, sum
+    cin >> A >> B;
+    X = A + B;
+    cout << "X = " << X << endl;
+    return 0;
+}
