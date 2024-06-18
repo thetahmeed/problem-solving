2518f1cc44fc4b4284290e5fea61bf45f5e5bfba
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-06-19 03:46:39 +0600

    Renamed to correct order

---
+++ beecrowd/Beginner/3_simple_sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, SOMA; // input1, input2, sum
+    cin >> A >> B;
+    SOMA = A + B;
+    cout << "SOMA = " << SOMA << endl;
+    return 0;
+}
---
+++ beecrowd/Beginner/4_simple_product.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, PROD; // PROD is the product of a and b
+    cin >> a >> b;
+    PROD = a * b;
+    cout << "PROD = " << PROD << endl;
+    return 0;
+}
