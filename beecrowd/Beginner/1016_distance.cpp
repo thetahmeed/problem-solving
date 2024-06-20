8d79b7a453d164ac7c5b4436a19315c9a5e94a61
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-06-21 00:06:15 +0600

    Create 1016_distance.cpp

---
+++ beecrowd/Beginner/1016_distance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int distance;
+    cin >> distance;
+
+    cout << distance * 2 << " minutos" << endl; // Distance * 2 minutes
+
+    return 0;
+}
